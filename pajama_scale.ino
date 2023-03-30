dd95f37f8967b41e645c45756ab1e1778f97b64e
j.danielse <j.danielse@bitbucket.com>
2023-03-30 15:22:57 +0200

    Update pajama_scale.ino
    init for virtual panel

--- pajama_scale.ino
+++ pajama_scale.ino
@@ -1,17 +1,41 @@
 #include <Q2HX711.h>
+#include "VirtualPanel.h"
 
-const byte data_pin = A2;
-const byte clock_pin = A3;
+const byte hx711_data_pin = A2;
+const byte hx711_clock_pin = A3;
 
-Q2HX711 hx711(data_pin, clock_pin);
+Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
 
-void setup()
+float Weight = 0.0;
+float Tare = 0.0;
+
+void setup() 
 {
-       Serial.begin(115200);
+  Panel.begin(); // init port and protocol
 }
 
-void loop()
+void loop() 
 {
-        Serial.println(hx711.read());
-        delay(50);
+  Panel.receive(); // handle panel events form the panel (must be in the loop)
+
+  Weight = ((float)hx711.read() / 1000.0) - Tare;
+  Panel.sendf(Display_1,"%s", _FString(Weight,1,0));
+  delay(500);
+}
+
+void PanelCallback(vp_channel event) 
+{ 
+  switch (event) 
+  {
+    case PanelConnected: // receive panel connected event
+      Panel.send(ApplicationName,"Pyama Scale"); 
+      Panel.send(Button_8, F("tare"));
+      break;
+
+    case Button_8:
+      Tare = Weight;
+
+    default: break;
+
+  }
 }
